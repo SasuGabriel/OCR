e95d6fbc1aeaa86f648a4a0f76a64f488282e8e5
IlincaBusuioc <ilinca.busuioc@web.de>
2020-11-22 21:18:58 +0200

    Update freq postags.py

--- TemaLaborator6/freq postags.py
+++ TemaLaborator6/freq postags.py
@@ -4,6 +4,9 @@ Created on Mon Nov 16 19:58:12 2020
 
 @author: pc
 """
+#Disclaimer: Output-urile "Camil Petrescu - Ultima noapte de dragoste, intaia noapte de razboi_Parser.xml" și "Camil Petrescu - Ultima noapte de dragoste, intaia noapte de razboi_Tagger.xml" sunt obținute dintr-un tagger și un parser astfel: 
+#Aici e parserul: https://www.dropbox.com/s/fbr7glgv9yz44sy/bin_FdgParserRo.zip?dl=0
+#Aici este pos tager-ul: https://www.dropbox.com/s/74gl7ubh3s10mcv/bin_PosRo.zip?dl=0
 
 import xml.etree.ElementTree as ET
 #Analizam fisierul XML rezultat in urma parsarii
