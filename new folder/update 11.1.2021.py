58e4c1a43009318b09481477b47135d1e2469f72
IlincaBusuioc <ilinca.busuioc@web.de>
2021-01-15 13:12:02 +0200

    Update update 11.1.2021.py

--- new folder/update 11.1.2021.py
+++ new folder/update 11.1.2021.py
@@ -10,53 +10,73 @@ with open('D:/BusuiocI/Desktop/134_4.json', encoding='utf-8') as f:
 text=data['fullTextAnnotation']['text'].replace('\n',' ')
 
 #READ FROM PDF
-PDF_file = 'D:/BusuiocI/Documents/Bibliografie/Anul 2, Sem I/Statistical processing of textual data/Baza de date Analiza text/Corpus-master-Onofrei Mihaela/Corpus-master/Pdf/aviz de insotire a marfii.pdf'
-import fitz
+from pdf2image import convert_from_path
+images = convert_from_path("D:/BusuiocI/Documents/Bibliografie/Anul 2, Sem I/Statistical processing of textual data/Baza de date Analiza text/Corpus-master-Onofrei Mihaela/Corpus-master/Pdf/factura_clasica.pdf", 500,poppler_path=r'D:/BusuiocI/Downloads/poppler-0.68.0_x86/poppler-0.68.0/bin')
+for i, image in enumerate(images):
+    fname = 'image'+str(i)+'.png'
+    image.save(fname, "PNG")
+
 import cv2
 import pytesseract
-doc=fitz.open(PDF_file)
-page=doc.loadPage(0)
-pix=page.getPixmap()
-output="outfile.PNG"
-pix.writePNG(output)
-img=cv2.imread("D:/BusuiocI/Documents/Bibliografie/Anul 2, Sem I/Statistical processing of textual data/Baza de date Analiza text/Facturi Analiza text-Ilinca/eon/20201023_130755.jpg")
+img=cv2.imread("D:/BusuiocI/Desktop/image0.png")
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 custom_config = r'--oem 3 --psm 12'
 text=pytesseract.image_to_string(img, config=custom_config, lang="ron")
-text=text.replace('\n',' ')
+img2=cv2.imread("D:/BusuiocI/Desktop/image1.png")
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+custom_config = r'--oem 3 --psm 12'
+text2=pytesseract.image_to_string(img2, config=custom_config, lang="ron")
+text=text+text2
+#text=text.replace('\n',' ')
 
 #REGEX UNIVERSAL
 import re
-telephone=re.findall("Număr telefon/cont: (0\d{3}\d{3}\d{3}\s)| (0\d{3}\d{3}\d{3}\s)|(0\d{4} \d{2} \d{2} \d{2}\s)| (0\d{9})\s", text)
-postalcodesender=re.findall("\s\d{6}\s", text)[0].strip()
-CIF=re.findall("\s[A-Z][A-Z]\d{6,10}\s|\s[A-Z][A-Z]\d{6,10}\s|\sR\d{9}\s", text)[0].strip()
+telephone=re.findall("Număr telefon/cont: (0\d{3}\d{3}\d{3}\s)| (0\d{3}\d{3}\d{3}\s)|(0\d{4} \d{2} \d{2} \d{2}\s)| (0\d{9})\s|Voce: ([0-9]{10})", text)
+postalcodesender=re.findall("\s\d{6}\s|\s\d{6}\,", text)
+CIF=re.findall("\s[A-Z][A-Z]\d{4,10}\s|\s[A-Z][A-Z]\d{6,10}\s|\sR\d{9}\s|\sR\d{9}|Cod de inregistrare fiscala: +([A-z0-9]{9})|CIF:\s([A-Z][A-Z]\d{4,10})", text)
 CIF
-seria=re.findall("Seria [A-Z]{4} nr. [0-9]{7}", text)
-seria=seria[0]
+seria=re.findall("Seria [A-Z]{4} nr. [0-9]{7}|SERIA [A-Z]{4} +NR. +[0-9]{11}\/[0-9]{4}\-[0-9]{2}-[0-9]{2}|Seria şi nr\.\:\s[A-Z0-9]{6}\-[A-Z]", text)
 numepersoana=re.findall("([A-Z\-]{4,})\s+([A-Z\-]{5,})??\s?([A-Z\-]{5,})\s", text)
-perioadafacturare=re.findall("[0-9]{2}\.[0-9]{2}\.[0-9]{4}\-[0-9]{2}\.[0-9]{2}\.[0-9]{4}|[0-9]{2}\.[0-9]{2}\.[0-9]{2}\s\-\s[0-9]{2}\.[0-9]{2}\.[0-9]{2}", text)[0].strip()
+perioadafacturare=re.findall("[0-9]{2}\.[0-9]{2}\.[0-9]{4}\-[0-9]{2}\.[0-9]{2}\.[0-9]{4}|[0-9]{2}\.[0-9]{2}\.[0-9]{2}\s\-\s[0-9]{2}\.[0-9]{2}\.[0-9]{2}|[0-9]{2}\.[0-9]{2}\.[0-9]{2}\-[0-9]{2}\.[0-9]{2}\.[0-9]{2}|[0-9]{2}\.[0-9]{2}\.[0-9]{4}\s\-\s[0-9]{2}\.[0-9]{2}\.[0-9]{4}", text)
 perioadafacturare
-datafacturare=re.findall("\Data facturării\:\s([0-9]{2}\.[0-9]{2}\.[0-9]{4})\s|Data:\s([0-9]{4}\-[0-9]{2}\-[0-9]{2})\s|Data emiterii +([0-9]{2}\.[0-9]{2}\.[0-9]{4})", text)[0]
-duedate=re.findall("Total de plată pană la\s([0-9]{2}\.[0-9]{2}\.[0-9]{4})|Scadent:\s([0-9]{4}\-[0-9]{2}\-[0-9]{2})\s|până la data de ([0-9]{2}\.[0-9]{2}\.[0-9]{4})", text)[0]
-totalplata=re.findall("(\-?\d+,\d{2})|\s(\d+\.\d{2})\s|Vă rugăm să achitaţi suma de\s(\-?\d+,\d{2})", text)
+datafacturare=re.findall("\Data facturării\:\s([0-9]{2}\.[0-9]{2}\.[0-9]{4})\s|Data:\s([0-9]{4}\-[0-9]{2}\-[0-9]{2})\s|Data emiterii +([0-9]{2}\.[0-9]{2}\.[0-9]{4})|Data facturii: ([0-9]{2}\.[0-9]{2}\.[0-9]{4})|din:\s([0-9]{2}\.[0-9]{2}\.[0-9]{4})\s|Data facturii: ([0-9]{2}\/[0-9]{2}\/[0-9]{4})\s", text)
+duedate=re.findall("Total de plată pană la\s([0-9]{2}\.[0-9]{2}\.[0-9]{4})|Scadent:\s([0-9]{4}\-[0-9]{2}\-[0-9]{2})\s|până la data de ([0-9]{2}\.[0-9]{2}\.[0-9]{4})|scadenta:\s([0-9]{2}\.[0-9]{2}\.[0-9]{4})\s|Data scadenţei: ([0-9]{2}\/[0-9]{2}\/[0-9]{4})", text)
+totalplata=re.findall("(\-?\d+,\d{2})\s|\s(\-?\d+\.\d{2})\s|Vă rugăm să achitaţi suma de\s(\-?\d+,\d{2})\s|TOTAL DE PLATA .*? (\-?\d+.\d{2})\s|(\-?\d+\.\d{3},\d{2})\s", text)
 totalplata2=[element for tupl in totalplata for element in tupl]
 without_empty_strings = [string for string in totalplata2 if string != ""]
-j=[]
-for i in without_empty_strings:
-    j.append(float(i.replace(',','.')))
-totalplata=max(j) #nu merge extraordinar, adica ia valoarea maxima care nu e tot timpul suma de platit
-IBAN=re.findall("Cod IBAN:\s([A-Z]{2}[0-9]{2}\s[a-zA-Z0-9]{4}\s[a-zA-Z0-9]{4}\s[a-zA-Z0-9]{4}\s[a-zA-Z0-9]{4}\s[a-zA-Z0-9]{4})\s|\s([A-Z0-9]{24})\s", text)
+#j=[]
+#for i in without_empty_strings:
+#    j.append(float(i.replace(',','.')))
+#totalplata=max(j)
+IBAN=re.findall("Cod IBAN:\s([A-Z]{2}[0-9]{2}\s[a-zA-Z0-9]{4}\s[a-zA-Z0-9]{4}\s[a-zA-Z0-9]{4}\s[a-zA-Z0-9]{4}\s[a-zA-Z0-9]{4})\s|\s([A-Z0-9]{24})\s|\s([A-Z0-9]{11}\s[0-9]{13})", text)
 IBAN
-codfacturare=re.findall("\s([0-9]{14})\s", text)[0]
-furnizor=re.findall("[0-9]+ (.*? S.A.)|FURNIZOR: (.*? SRL)|(FIRMA .*?) Cumparator|(.*? SA)", text)
-adresafurnizor=re.findall("(Oras:.*?)Cui", text)
+codfacturare=re.findall("\s([0-9]{14})\s", text)
+furnizor=re.findall("[0-9]+ (.*? S.A.)|FURNIZOR: (.*? SRL)|(FIRMA .*?) Cumparator|(.*? SA)|(.*? SRL)|(.*? S.A.)", text)
+adresafurnizor=re.findall("(Oras:.*?)Cui|SRL (.*?) Capital social|S.A. (.*?) FACTURA", text)
+adresadestinatar=re.findall("Adresa: (.*?)suma de|([A-Z]+\.[A-Z]+ NR.[0-9]{2,4}[A-Z])", text)
 produse=re.findall("Produse si servicii facturate (.*?) Suma facturata \(fara TVA\)|Denumire\/Produs\/Servicii (.*?)\s\d+\.\d{2}\s", text)
-codclient=re.findall("COD CLIENT: ([0-9]{9})|Cod abonat: ([0-9]{10})", text)[0]
-nrfacturii=re.findall("Nr. Facturii: ([0-9]{8})|Nr. factură: ([0-9]{13})", text)[0]
+codclient=re.findall("COD CLIENT: ([0-9]{9})|Cod abonat: ([0-9]{10})|Cod client: ([0-9]{8})", text)
+nrfacturii=re.findall("Nr. Facturii: ([0-9]{8})|Nr. factură: ([0-9]{13})|Numar factura. ([A-Z0-9]{6} [0-9]{6})|Nr. ([0-9]{8})", text)
+punctdelucru=re.findall("PUNCT DE LUCRU: (.*?) CHITANTA", text)
+rambursla=re.findall("ramburs la ([A-Z]+ [0-9]+)", text)
+clientulexpeditor=re.findall("(clientul .*?) Tiparit",text)
+amprimitdela=re.findall("Am primit de la +: (.*?) CIF|Am primit de la +: (.*?) Adresa", text)
+
+from prettytable import PrettyTable
+camp=['CIF','furnizor','codclient','codfacturare','datafacturare','duedate','furnizor','nrfacturii','numepersoana','perioadafacturare',
+      'totalplata', 'seria',
+      'adresadestinatar','adresafurnizor', 'punctdelucru', 'rambursla', 'clientulexpeditor', 'amprimitdela']
+valoare=[CIF,furnizor,codclient,codfacturare,datafacturare,duedate,furnizor,nrfacturii,numepersoana,perioadafacturare,
+      totalplata, seria,
+      adresadestinatar,adresafurnizor, punctdelucru, rambursla, clientulexpeditor, amprimitdela]
+table=PrettyTable(['camp','valoare'])
+for x in range(0,len(camp)):
+    table.add_row([camp[x],valoare[x]])
+print(table)
 
 #SPACY NLP
 import spacy
-nlp = spacy.load('ro_core_news_lg')
+nlp = spacy.load('ro_core_news_md') #sm, lg
 doc=nlp(text)
 
 #ELIMINARE TOKENURI CARE NU SUNT DIN DEXONLINE
@@ -66,7 +86,6 @@ import requests
 def DefinitieCuvant(Cuvant, NumarDefinitii = 0,  TipDictionar = 0, encoding="utf-8"):
     WordPage = "https://dexonline.ro/definitie/" + str(Cuvant) + "/expandat"
     Tree = html.fromstring(requests.get(WordPage).content)
-
     Words = Tree.xpath('/html/body/div[1]/main/div/div/div[1]/div[2]/p/span[1]/text()')
     return(Words)
 lemma=[]
@@ -91,7 +110,6 @@ def show_entsproduct(doc):
     else: print('No named entities found.')
 print("Produsele oferite sunt: ")
 show_entsproduct(doc)
-
 def show_entsmoney(doc):
     if doc.ents:
             for ent in doc.ents:
@@ -177,25 +195,11 @@ def show_entsall(doc):
     else: print('No named entities found.')
 show_entsall(doc)
 
-#PRETTYTABLE
-from prettytable import PrettyTable
-camp=['CIF','numepersoana','perioada facturare','data facturare', 'total de plata pana la: ','IBAN',
-      'cod facturare','telefon/cont','total plata', 'seria', 'adresa furnizor',
-      'adresa destinatar']
-valoare=[CIF,numepersoana,perioadafacturare,datafacturare,duedate,IBAN,
-      codfacturare,telephone,totalplata, seria,adresafurnizor,
-      adresadestinatar]
-table=PrettyTable(['camp','valoare'])
-for x in range(0,len(camp)):
-    table.add_row([camp[x],valoare[x]])
-print(table)
-
-
 #EXPORT TABLE TO CSV
 data = table.get_string()
 import csv
 result = [tuple(filter(None, map(str.strip, splitline))) for line in data.splitlines() for splitline in [line.split("|")] if len(splitline) > 1]
-with open('output.csv', 'w', newline='') as outcsv:
+with open('output.csv', 'w', newline='', encoding="utf-8") as outcsv:
     writer = csv.writer(outcsv)
     writer.writerows(result)
     
